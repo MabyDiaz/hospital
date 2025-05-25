c655ecfb8cd2d180d0f7590fcf6a4de3b5af7960
Ignacio Gabriel Brasesco <brasescoignaciogabriel@gmail.com>
2025-05-25 16:40:50 -0300

    edite tratamientoController

--- src/controllers/tratamientoController.js
+++ src/controllers/tratamientoController.js
@@ -23,3 +23,40 @@ export const getTratamientos = async(req, res) => {
         res.status(500).json( {error: 'Error al obtener tratamientos'})
     }
 }
+
+
+
+export const getTratamientosById = async(req, res) => {
+    try {
+        const { cuit } = req.params;
+        const tratamiento = await Tratamiento.findByPk(Id, {
+            include:[
+                    {
+                        model: HistorialMedico,
+                        attributes: ['id']
+                    },
+                    {
+                        model: Medico,
+                        attributes: ['id']
+                    }
+
+                    ]
+        }); 
+        
+         if (tratamiento) {
+            res.status(200).json(tratamiento);
+        } else {
+            res.status(404).json({ message: 'tratamiento no encontrado' });
+        }
+
+        
+    } catch (error) {
+        res.status(500).json( {error: 'Error al obtener el tratamiento'})
+    }
+}
+
+// Crear
+
+// modificar
+
+// eliminar
